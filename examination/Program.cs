3fb79406deda18e7c0555e8c99730957e6663cf8
grecheskiy <grecheskiy@rambler.ru>
2023-02-24 23:16:48 +0300

    added void FillMass

--- examination/Program.cs
+++ examination/Program.cs
@@ -1,3 +1,8 @@
-﻿string[] array = new string[5];
+﻿void FillMass(string[] array)
+{for(int i = 0; i < array.Length; i++)
+{array[i] = Console.ReadLine() ?? "";}}
+
+string[] array = new string[5];
 string[] three = new string[array.Length];
 Console.Write("Enter any 5 words through <Enter>: ");
+FillMass(array);
