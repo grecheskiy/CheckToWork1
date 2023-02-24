1acaefa0171349ba83863618ab893357bc51f82d
grecheskiy <grecheskiy@rambler.ru>
2023-02-24 23:24:23 +0300

    added void PrintMass

--- examination/Program.cs
+++ examination/Program.cs
@@ -2,7 +2,12 @@
 {for(int i = 0; i < array.Length; i++)
 {array[i] = Console.ReadLine() ?? "";}}
 
+void PrintMass(string[] array)
+{for(int i = 0; i < array.Length; i++)
+{Console.Write($"{array[i]} ");}}
+
 string[] array = new string[5];
 string[] three = new string[array.Length];
 Console.Write("Enter any 5 words through <Enter>: ");
 FillMass(array);
+PrintMass(array);
